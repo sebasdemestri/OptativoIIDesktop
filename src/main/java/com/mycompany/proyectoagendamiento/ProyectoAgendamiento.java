3eb032d34c7e6aa511e75b446fc443c67ace99d1
UA\lab217-17 <sebastiandemestri10@gmail.com>
2023-09-04 21:35:02 -0400

    Big bang

---
+++ src/main/java/com/mycompany/proyectoagendamiento/ProyectoAgendamiento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoagendamiento;
+
+/**
+ *
+ * @author lab217-17
+ */
+public class ProyectoAgendamiento {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
